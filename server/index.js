26cdd91b964cc109a70c1198e341c9aa945332d1
“Abhishek <kabhi12132002@gmail.com>
2024-05-01 17:39:14 +0530

    added vercel.json

--- server/index.js
+++ server/index.js
@@ -2,39 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const mongoose = require('mongoose');
-const displayFoodData = require('./routes/displayFoodData.js');
+const connectDB = require("./config/db");
 
+//env config
 dotenv.config();
 
-// Connect to MongoDB
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connected to MongoDB Database');
-  } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
-  }
-};
-connectDB(); // Call connectDB function to establish MongoDB connection
+//router import
+const displayFoodData = require('./routes/displayFoodData.js')
 
+//mongodb connection
+connectDB();
+
+//rest objecct
 const app = express();
 
+// Allow requests from your React app's origin
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, // This allows cookies to be sent with the request
+};
+
+// middlewares
+// app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// app.use('/foods', displayFoodData);
-app.use('/',async(req,res)=>{
-  res.status(200).json({
-    message:"okay"
-  })
-})
+//routes
+app.use('/api/v1/foods', displayFoodData)
 
+
+// Port
 const PORT = process.env.PORT || 8080;
-
-app.listen(5000, () => {
-  console.log(`Server running`);
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on  portt no ${PORT}`
+  );
 });
